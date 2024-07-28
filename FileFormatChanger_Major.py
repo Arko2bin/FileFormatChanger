7e3d99f7dc5c896d2fdd2010e317e0ef884e8f43
Arko Mukherjee <72858291+Arko2bin@users.noreply.github.com>
2024-07-28 18:14:16 +0530

    Update FileFormatChanger_Major.py
    Warning message updated

--- FileFormatChanger_Major.py
+++ FileFormatChanger_Major.py
@@ -158,7 +158,7 @@ def Cut_Videos(video,start_time,end_time):
             return True
 
 with st.container():
-    st.warning(":warning: Downloading from youtube is currently disabled by youtube. We are waiting for further updates from youtube. Till then you can use our desktop app for downloading from youtube")
+    st.success(":warning: Downloading from youtube is currently disabled by youtube. We are waiting for further updates from youtube. Till then you can use our desktop app for downloading from youtube")
     st.title("Format Changer App")
     st.header("Hi I am your app to change the format of your files: ")
     st.write("---")
